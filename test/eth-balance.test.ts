1ee4d898f7b11ce8233ad934d837ca0f084fb267
Shravan Sunder <ShravanSunder@users.noreply.github.com>
2022-05-29 19:18:46 -0400

    #2 copied over more repo settings (#4)
    * #2 copied over more repo settings
    
    * #2 removed hardhat ts
    
    * #2 fix eslint issues
    
    * #2 fix eslint issues

--- test/eth-balance.test.ts
+++ test/eth-balance.test.ts
@@ -2,7 +2,7 @@ import 'test/helpers/chai-imports';
 import { expect } from 'chai';
 
 describe('useEthBalance', function () {
-  it('When a test is run mocha and chai work', async () => {
+  it('When a test is run mocha and chai work', () => {
     expect(true).be.equal(true);
   });
-})
+});
